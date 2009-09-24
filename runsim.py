f7e5f7d47e4ebf25a36a47bb2554d7d0b37ffba7
Daniel Johnson <2010djohnson@tjhsst.edu>
2009-09-24 13:28:57 -0400

    make sim take time to run

--- runsim.py
+++ runsim.py
@@ -1,8 +1,13 @@
 import sim
-
+from time import sleep
+def step():
+	sim.local.actall()
+	sleep(1)
 n1=sim.Node("n1")
 n2=sim.Node("n2")
 n1.sendmsg(n2,"m:hi!")
+step()
 n2.multicast("m:hihi!")
+step()
 n1.sendmsg(n2,"ping:hi")
-sim.local.actall()
+step()
