3601a4657ce8d03fae3ed94fd4b905fa4f3c6ccf
Daniel Johnson <2010djohnson@tjhsst.edu>
2009-12-15 12:58:08 -0500

    add multicast code to "sim"

--- runsim.py
+++ runsim.py
@@ -5,20 +5,22 @@ def step():
 	sim.local.actall()
 	sleep(1)
 n1=sim.Node("n1")
-n2=sim.Node("n2")
-n1.sendmsg(n2,"m:hi!")
+#n2=sim.Node("n2")
+#n1.sendmsg(n2,"m:hi!")
+#step()
+#n2.multicast("m:hihi!")
+#step()
+#n1.sendmsg(n2,"ping:hi")
+#step()
+#n1.ismaster=True
+#n3=sim.Node("n3")
+#step()
+#list=[0]*10
+#for n in xrange(10):
+#	list[n]=sim.Node("node%d"%n)
 step()
-n2.multicast("m:hihi!")
-step()
-n1.sendmsg(n2,"ping:hi")
-step()
-n1.ismaster=True
-n3=sim.Node("n3")
-step()
-list=[0]*10
-for n in xrange(10):
-	list[n]=sim.Node("node%d"%n)
-step()
+while True:
+	step()
 #while True:
 #	line=raw_input()
 #	try:
--- sim.py
+++ sim.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 from time import time
+from random import shuffle
+from socket import *
+import struct
+MYGROUP = '225.0.0.250'
 
 class Link:
 	def __init__(self):
 		self.nodes=[]
+		self.msendsocket=socket(AF_INET, SOCK_DGRAM)
+		ttl = struct.pack('b', 1)
+		self.msendsocket.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, ttl)
+
 	def addnode(self,n):
 		self.nodes.append(n)
 	def multicast(self,sender,msg):
-		for n in self.nodes:
-			n.recvmsg(sender,msg)
+#		for n in self.nodes:
+#			n.recvmsg(sender,msg)
+		self.msendsocket.sendto(msg, (MYGROUP, 81234))
 	def actall(self):
 		anything=True
 		while anything:
@@ -17,7 +26,53 @@ class Link:
 				if n.act():
 					anything=True
 
+def openmcastsocket(group, port):
+    # Import modules used only here
+    import string
+    import struct
+    #
+    # Create a socket
+    s = socket(AF_INET, SOCK_DGRAM)
+    #
+    # Allow multiple copies of this program on one machine
+    # (not strictly needed)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    #
+    # Bind it to the port
+    s.bind(('', port))
+    #
+    # Look up multicast group address in name server
+    # (doesn't hurt if it is already in ddd.ddd.ddd.ddd format)
+    group = gethostbyname(group)
+    #
+    # Construct binary group address
+    bytes = map(int, string.split(group, "."))
+    grpaddr = 0
+    for byte in bytes: grpaddr = (grpaddr << 8) | byte
+    #
+    # Construct struct mreq from grpaddr and ifaddr
+    ifaddr = INADDR_ANY
+    mreq = struct.pack('ll', htonl(grpaddr), htonl(ifaddr))
+    #
+    # Add group membership
+    s.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+    #
+    s.setblocking(0)
+    return s
+
 local = Link()
+def getslot(record):
+	parts=record.split(".")
+	domain="%s.%s"%(parts[-2],parts[-1])
+	#print "DEBUG: domain: %s"%domain
+	import hashlib
+	hasher = hashlib.sha1()
+	hasher.update(domain)
+	hash=hasher.hexdigest()
+	#print "DEBUG: hash: %s"%hash
+	slot=hash[:2]
+	#print "DEBUG: slot: %s"%slot
+	return slot
 
 class Node:
 	def __init__(self,uid,link=local):
@@ -25,6 +80,8 @@ class Node:
 		self.msgqueue=[]
 		self.rcvhistory=[]
 		self.records={}
+		self.addressmap={}
+		self.mrecvsocket = openmcastsocket("225.0.0.250", 81234)
 		self.online=True
 		self.link=link
 		self.link.addnode(self)
@@ -33,11 +90,18 @@ class Node:
 		self.state="starting"
 		self.ismaster=False
 		self.lasttime=time()
+		self.reqlist=[]
+	def assignmaps(self):
+		for n in xrange(16**2):
+			val=hex(n)[2:]
+			if val not in addressmap:
+				addressmap[val]=[]
+
 	def recvmsg(self,sendernode,msg):
 		if self.online:
 			self.msgqueue.append([msg,sendernode])
 	def sendmsg(self,targetnode,msg):
-		targetnode.recvmsg(self,msg)
+		print "sendmsg not implemented"
 	def multicast(self,msg):
 		self.link.multicast(self,msg)
 	def sendlist(self):
@@ -55,34 +119,65 @@ class Node:
 				deleg[n].append(temppos)
 				temppos+=LENSPACE/len(hostlist)
 			print "%s:%s"%(hostlist[n],deleg[n])
+	def lookupw(self):
+		slot = self.reqlist[0][0]
+		record = self.reqlist[0][1]
+		type = self.reqlist[0][2]
+		if slot in addressmap:
+			#TODO: query the hosts in a random order
+			l=addressmap[slot]
+			shuffle(l)
+			for h in l:
+				pass
+				#TODO do query
 	def lookup(self, type, record):
 		if record in records:
 			return records[record]
 		else:
-			self.multicast("dig:%s:%s"%(type,record))
+			slot=getslot(record)
+			self.multicast("findgroup:%s"%slot)
+			self.reqlist.append((slot,record,type,time()))
+	def fillqueue(self):
+		try:
+			while True:
+				data, sender = self.mrecvsocket.recvfrom(1500)
+				while data[-1:] == '\0': data = data[:-1] # Strip trailing \0's
+				print "sender",sender
+				self.recvmsg(sender[0],data)
+		except error:
+			pass
 	def act(self):
 		if self.online:
 			try:
+				self.fillqueue()
 				msg=self.msgqueue.pop(0)
+				text=msg[0]
+				try:
+					sender=msg[1].guid
+				except AttributeError:
+					sender=msg[1]
 				self.rcvhistory.append(msg)
-				print "#%s got a msg from %s: %s"%(self.guid,msg[1].guid,msg[0])
-				firstpart=msg[0].split(":")[0]
+				try:
+					print "#%s got a msg from %s: %s"%(self.guid,sender,text)
+				except AttributeError:
+					print "#%s got an anon msg: %s"%(self.guid,text)
+				firstpart=text.split(":")[0]
 				if firstpart=="m":
-					print "===== %s says %s ====="%(msg[1].guid,msg[0].split(":")[1])
+					print "===== %s says %s ====="%(sender,text.split(":")[1])
 				elif firstpart=="ping":
-					self.sendmsg(msg[1],"pong:%s"%msg[0].split(":")[1])
+					self.sendmsg(sender,"pong:%s"%text.split(":")[1])
 				elif firstpart=="join":
 					if self.ismaster:
-						if msg[1].guid not in self.hostlist:
-							self.hostlist.append(msg[1].guid)
+						if sender not in self.hostlist:
+							self.hostlist.append(sender)
 							self.dirtylist = True
 				elif firstpart=="complain":
 					if self.ismaster:
-						if msg[1].guid not in self.hostlist:
-							self.hostlist.append(msg[1].guid)
+						if sender not in self.hostlist:
+							self.hostlist.append(sender)
 							self.sendlist()
 				elif firstpart=="list":
-					newlist=msg[0].split(":")[1].split("\n")[:-1]
+					newlist=text.split(":")[1].split("\n")[:-1]
 					if self.guid in newlist:
 						list=newlist
 						if self.state=="joining":
@@ -90,7 +185,7 @@ class Node:
 					else:
 						self.multicast("complain:%s"%self.guid)
 				elif firstpart=="dig":
-					hname = msg[0].split(":")[2]
+					hname = text.split(":")[2]
 					if hname in records:
 						self.sendmsg(msg[1],"ans:%s"%records[hname])
 
@@ -115,6 +210,8 @@ class Node:
 						if self.dirtylist:
 							self.sendlist()
 							self.dirtylist=False
+				if len(self.reqlist)>0:
+					self.lookupw()
 				return False
 			return True
 		return False
